f4475163a1d147ebec094c11e34111c1786e9dc6
Ankur Rawat <ankurrawat620@gmail.com>
2023-03-19 03:36:54 +0530

    Create NewsItem/News Components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import React, { Component } from 'react'
+import Navbar from './components/Navbar';
+import News from './components/News';
 
-class App extends Component {
+export default class App extends Component {
   render() {
     return (
-      <div>App</div>
+      <div>
+        <Navbar />
+        <News />
+      </div>
     )
   }
 }
-
-export default App;
