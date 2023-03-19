cb5657e068c5410f2271f278f5f150c9697492a9
Ankur Rawat <ankurrawat620@gmail.com>
2023-03-19 13:08:10 +0530

    Local News

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ export default class App extends Component {
     return (
       <div>
         <Navbar />
-        <News />
+        <News headline="Top Headlines"/>
       </div>
     )
   }
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react'
 
 export class NewsItem extends Component {
+
   render() {
+    let {title, description, imgUrl, newsUrl} = this.props;
     return (
-      <div>This is a news item</div>
+      <div className="my-3">
+        <div className="card" style={{width: "18rem"}}>
+          <img src={imgUrl} className="card-img-top newsImage" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">{description}.</p>
+            <a href={newsUrl} className="btn btn-outline-success btn-sm" target="_blank">Read More</a>
+          </div>
+        </div>
+      </div>
     )
   }
 }
